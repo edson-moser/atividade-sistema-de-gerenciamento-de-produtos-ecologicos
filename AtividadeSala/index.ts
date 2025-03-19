ad5b5d48897558767744c574fca623b4bae0039f
edson-moser <edsonmoser97@gmail.com>
2025-03-19 08:52:50 -0300

    edsonMoser

---
+++ AtividadeSala/index.ts
@@ -0,0 +1,18 @@
+let nome: String="edson";
+console.log(nome)
+
+var pi=3.14;
+const idade: number=20;
+
+console.log(pi);
+
+function saudacao(nome,idade){
+}
+
+var nomeUsuario="edson";
+var idadeOld=22;
+saudacao(nomeUsuario,idadeOld);
+
+var pessoa={
+    
+}
